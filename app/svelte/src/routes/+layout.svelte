d8927d48b7e11ec57a7911b4c3c31632f788c441
unknown <nick.a.trudeau@gmail.com>
2024-06-21 13:59:38 -0700

    svelte app add

---
+++ app/svelte/src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { isOverlayOpen } from './../stores/Overlay.js';
+	import '../app.css';
+	import Overlay from '$lib/components/Overlay.svelte';
+</script>
+
+<header>
+	{#if $isOverlayOpen}
+		<Overlay>
+			<div class="bg-whisper sm:rounded-lg">
+				<div class="px-4 py-5 sm:p-6">
+					<h3 class="text-base font-semibold leading-6 text-gray-900">
+						Provide the email associated with your account
+					</h3>
+					<form class="mt-5 sm:flex sm:items-center z-0">
+						<div class="w-full sm:max-w-xs">
+							<label for="email" class="sr-only">Email</label>
+							<input
+								type="email"
+								name="email"
+								id="email"
+								class="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								placeholder="email@example.com"
+								required
+							/>
+						</div>
+						<button
+							type="submit"
+							class="mt-3 inline-flex w-full items-center justify-center rounded-md bg-nivaltaBlue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto"
+							>Submit</button
+						>
+					</form>
+				</div>
+			</div>
+		</Overlay>
+	{/if}
+</header>
+
+<div class="grid min-h-screen grid-cols-12 bg-nivaltaBlue">
+	<main class="col-span-12">
+		<slot />
+	</main>
+</div>
---
+++ app/svelte/src/routes/login/signup/+page.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="flex h-screen w-screen justify-center items-center">
+	<div class="flex bg-whisper h-[600px] w-[400px] rounded-xl items-center justify-center">
+		<form>
+			<div class="flex items-center">
+				<h1 class="text-nivaltaBlue font-bold text-xl">Sign Up</h1>
+			</div>
+			<div class="space-y-12">
+				<div class="border-b border-gray-900/10 pb-12">
+					<div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+						<div class="sm:col-span-3">
+							<label for="first-name" class="block text-sm font-medium leading-6 text-gray-900"
+								>First name</label
+							>
+							<div class="mt-2">
+								<input
+									type="text"
+									name="first-name"
+									id="first-name"
+									autocomplete="given-name"
+									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div class="sm:col-span-3">
+							<label for="last-name" class="block text-sm font-medium leading-6 text-gray-900"
+								>Last name</label
+							>
+							<div class="mt-2">
+								<input
+									type="text"
+									name="last-name"
+									id="last-name"
+									autocomplete="family-name"
+									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div class="sm:col-span-4 lg:col-span-6">
+							<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+								>Email address</label
+							>
+							<div class="mt-2">
+								<input
+									id="email"
+									name="email"
+									type="email"
+									autocomplete="email"
+									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div class="sm:col-span-4 lg:col-span-6">
+							<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+								>Password</label
+							>
+							<div class="mt-2">
+								<input
+									id="password"
+									name="password"
+									type="password"
+									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div class="sm:col-span-4 lg:col-span-6">
+							<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+								>Confirm Password</label
+							>
+							<div class="mt-2">
+								<input
+									id="password"
+									name="password"
+									type="password"
+									class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="mt-6 flex items-center justify-end gap-x-6">
+				<button
+					type="button"
+					class="text-sm font-semibold leading-6 text-gray-900"
+					on:click={() => goto('/')}>Cancel</button
+				>
+				<button
+					type="submit"
+					class="rounded-md bg-nivaltaBlue px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>Save</button
+				>
+			</div>
+		</form>
+	</div>
+</div>
