af2824ff44a09225eee2a8e46252435834bd8521
Brandon Lefore <brandon.lefore@nivalta.com>
2024-06-21 21:50:13 -0700

    Remove old falcon objects

--- app/api/main.py
+++
@@ -1,9 +0,0 @@
-import falcon
-from api.book import BookResource
-from resources.book_resource import BookResource as TestResource
-
-app = application = falcon.App()
-
-app.add_route("/books/fetch", TestResource())
-app.add_route("/books", BookResource())
-# app.add_route("/books/add", BookResource())
