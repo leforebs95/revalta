35081fd52a10c86c543b624114373106b95631e2
unknown <nick.a.trudeau@gmail.com>
2024-07-29 15:06:16 -0700

    added routes to signup and login buttons

--- app/sveltekit/src/routes/+page.svelte
+++ app/sveltekit/src/routes/+page.svelte
@@ -41,7 +41,7 @@
 				<a href="/" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
 			</div>
 			<div class="hidden lg:flex lg:flex-1 lg:justify-end">
-				<a href="/login" class="text-sm font-semibold leading-6 text-gray-900">Sign In </a>
+				<a href="/signup" class="text-sm font-semibold leading-6 text-gray-900">Sign In </a>
 				<a href="/login" class="text-sm font-semibold leading-6 text-gray-900"
 					>Log in <span aria-hidden="true">&rarr;</span></a
 				>
@@ -103,12 +103,12 @@
 						</div>
 						<div class="py-6">
 							<a
-								href="/"
+								href="/signup"
 								class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>Sign Up</a
 							>
 							<a
-								href="/"
+								href="/login"
 								class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>Log in</a
 							>
