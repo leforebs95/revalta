54be704cbd10fcad61173721888f047a6454d80b
unknown <nick.a.trudeau@gmail.com>
2024-07-29 14:56:46 -0700

    added sign up to navbar

--- app/sveltekit/src/routes/+page.svelte
+++ app/sveltekit/src/routes/+page.svelte
@@ -104,6 +104,13 @@
 							<a
 								href="/"
 								class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>Sign Up</a
+							>
+						</div>
+						<div class="py-6">
+							<a
+								href="/"
+								class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>Log in</a
 							>
 						</div>
