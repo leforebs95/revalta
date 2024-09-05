92ac59e7e9ce5781b2220aa8a16892999f4005ec
Brandon Lefore <brandon.lefore@nivalta.com>
2024-09-05 13:35:05 -0700

    Some changes, remove svelte?

--- app/svelte/.svelte-kit/output/client/_app/immutable/entry/start.JM1caTYC.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CFcsMsAF.js";export{t as start};
--- app/svelte/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/login/forgotpassword/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.BiXPX3lT.js","_app/immutable/chunks/scheduler.Bmg8oFKD.js","_app/immutable/chunks/index.B7858mxG.js","_app/immutable/chunks/entry.CFcsMsAF.js","_app/immutable/chunks/index.D_GRTHN4.js"];
-export const stylesheets = [];
-export const fonts = [];
