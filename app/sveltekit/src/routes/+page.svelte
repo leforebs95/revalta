a1601d65045fd09ebaeec9f01c2e1fe6dba986f6
Nick Trudeau <nick.a.trudeau@gmail.com>
2024-09-15 06:06:56 -0700

    removed navbar max width parameter

--- app/sveltekit/src/routes/+page.svelte
+++ app/sveltekit/src/routes/+page.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <header class="bg-white">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+    <nav class="mx-auto flex items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
-        <a href="/" class="-m-1.5 p-1.5 text-nivaltaBlue">Nivalta</a>
+        <a href="/" class="-m-1.5 p-1.5 text-nivaltaBlue font-bold text-3xl">Nivalta</a>
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
-        <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
+        <a href="/" class="text-md font-semibold leading-6 text-gray-900">Home</a>
         <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
         <a href="/" class="text-sm font-semibold leading-6 text-gray-900">About Us</a>
         <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Contact</a>
