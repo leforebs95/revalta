7c6fc7a82f6bad846694cebaf482a2e900d0aec7
Nick Trudeau <nick.a.trudeau@gmail.com>
2024-09-15 05:45:44 -0700

    changed login to wait list

--- app/sveltekit/src/routes/+page.svelte
+++ app/sveltekit/src/routes/+page.svelte
@@ -15,7 +15,7 @@
         <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Contact</a>
         </div>
         <div class="flex flex-1 items-center justify-end gap-x-6">
-        <a href="/" class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">Log in</a>
+        <a href="/" class="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">Wait List</a>
         <a href="/" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</a>
         </div>
         <div class="flex lg:hidden">
