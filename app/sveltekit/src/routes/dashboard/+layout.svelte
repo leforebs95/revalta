981fc92c07af60e9a19efb279607fc002d82b27f
Brandon Lefore <brandon.lefore@nivalta.com>
2024-09-26 13:33:28 -0700

    Alt for icon img

--- app/sveltekit/src/routes/dashboard/+layout.svelte
+++ app/sveltekit/src/routes/dashboard/+layout.svelte
@@ -18,7 +18,7 @@
 
 
 <div class="DashboardLeftNav w-[1536px] h-[1042px] relative bg-white rounded-[10px]">
-  <img class="Nivalta13 w-[173px] h-[173px] left-[26px] top-[-24px] absolute" src="/images/nivalta-logo.svg" />
+  <img class="Nivalta13 w-[173px] h-[173px] left-[26px] top-[-24px] absolute" src="/images/nivalta-logo.svg" alt="Nivalta Company Logo" />
   
   <div class="Frame10 w-[216px] h-[337px] left-[41px] top-[149px] absolute flex-col justify-start items-start gap-2.5 inline-flex">
     <div class="Frame11 w-[146px] py-2 justify-start items-center gap-2.5 inline-flex">
@@ -28,7 +28,7 @@
       <div class="Btn w-[146px] py-2 rounded-[40px] justify-start items-center gap-3 inline-flex">
         <div class="VuesaxLinearDirectInbox w-4 h-4 justify-center items-center flex">
           <div class="DirectInbox w-4 h-4 relative">
-			<img src="/images/vuesax-linear-direct-inbox.svg">
+			<img src="/images/vuesax-linear-direct-inbox.svg" alt="An Inbox Symbol">
           </div>
         </div>
         <div class="Dashboard grow shrink basis-0 text-[#202020] text-base font-normal font-['Poppins']">
@@ -39,7 +39,7 @@
         <div class="VuesaxLinearFolderOpen w-4 h-4 justify-center items-center flex">
           <div class="FolderOpen w-4 h-4 relative">
 			<a href="/dashboard/symptoms">
-				<img src="/images/vuesax-linear-open-folder.svg">
+				<img src="/images/vuesax-linear-open-folder.svg" alt="An Open Folder Icon">
 			</a>
           </div>
         </div>
@@ -50,7 +50,7 @@
         <div class="VuesaxLinearTaskSquare w-4 h-4 justify-center items-center flex">
           <div class="TaskSquare w-4 h-4 relative">
 			<a href="/dashboard/lab-results">
-				<img src="/images/vuesax-linear-task-square.svg">
+				<img src="/images/vuesax-linear-task-square.svg" alt="A Task List Icon">
 			</a>
 		  </div>
         </div>
@@ -61,7 +61,7 @@
       <div class="Btn w-[146px] py-2 rounded-[40px] justify-start items-center gap-3 inline-flex">
         <div class="VuesaxLinearPeople w-4 h-4 justify-center items-center flex">
           <div class="People w-4 h-4 relative">
-				<img src="/images/vuesax-linear-people.svg">
+				<img src="/images/vuesax-linear-people.svg" alt="A group of 3 people icons">
             <div class="Group w-[4.43px] h-[8.35px] left-[10.22px] top-[1.33px] absolute">
             </div>
             <div class="Group w-[4.42px] h-[8.35px] left-[1.34px] top-[1.33px] absolute">
@@ -75,7 +75,7 @@
       <div class="Btn w-[146px] py-2 rounded-[40px] justify-start items-center gap-3 inline-flex">
         <div class="VuesaxLinearPeople w-4 h-4 justify-center items-center flex">
           <div class="People w-4 h-4 relative">
-			<img src="/images/vuesax-linear-people.svg">
+			<img src="/images/vuesax-linear-people.svg" alt="A group of 3 people icons">
             <div class="Group w-[4.43px] h-[8.35px] left-[10.22px] top-[1.33px] absolute">
             </div>
             <div class="Group w-[4.42px] h-[8.35px] left-[1.34px] top-[1.33px] absolute">
@@ -95,7 +95,7 @@
     <div class="Btn self-stretch py-2 rounded-[40px] justify-start items-center gap-3 inline-flex">
       <div class="VuesaxLinearSetting2 w-4 h-4 justify-center items-center flex">
         <div class="Setting2 w-4 h-4 relative">
-			<img src="/images/vuesax-linear-setting2.svg">
+			<img src="/images/vuesax-linear-setting2.svg" alt="A settings gear">
         </div>
       </div>
       <div class="Dashboard grow shrink basis-0 text-[#202020] text-base font-bold font-['Poppins']">Settings</div>
@@ -103,7 +103,7 @@
     <div class="Btn self-stretch py-2 rounded-[40px] justify-start items-center gap-3 inline-flex">
       <div class="VuesaxLinearLogout w-4 h-4 justify-center items-center flex">
         <div class="Logout w-4 h-4 relative">
-			<img src="/images/vuesax-linear-logout.svg">
+			<img src="/images/vuesax-linear-logout.svg" alt="A logout icon">
 		</div>
       </div>
       <div class="Dashboard grow shrink basis-0 text-[#f13e3e] text-base font-medium font-['Poppins']">
