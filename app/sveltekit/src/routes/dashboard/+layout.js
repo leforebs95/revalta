8c2e5f20bcd9c316bb98bbaf67854095854caf13
Brandon Lefore <brandon.lefore@nivalta.com>
2024-09-27 21:52:24 -0700

    Set ssr to false globally

--- app/sveltekit/src/routes/dashboard/+layout.js
+++ app/sveltekit/src/routes/dashboard/+layout.js
@@ -1,5 +1,6 @@
 import { callFlaskEndpoint } from '$lib/session_data';
 
+/** @type {import('./$types').LayoutLoad} */
 export async function load({ fetch }) {
     let sessionData = await callFlaskEndpoint(fetch, '/api/getsession', 'GET');
     return {
--- app/sveltekit/src/routes/login/+page.js
+++ app/sveltekit/src/routes/login/+page.js
@@ -1,7 +1,5 @@
 import { callFlaskEndpoint } from '$lib/session_data';
 
-export const ssr = false;
-
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
     const sessionData = await callFlaskEndpoint(fetch, "/api/getsession", "GET");
