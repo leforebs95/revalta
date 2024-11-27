04cdbcd5840d82b19db02ac3cd5137945db69477
Brandon Lefore <brandon.lefore@nivalta.com>
2024-11-27 14:46:35 -0800

    Split services (#35)
    * Remove flask from nginx
    
    * More updates, ensure alignment with repo
    
    * checkpoint
    
    * INfra as code with cfn files
    
    * Got up to the services working
    
    * Stack succesfully deploys

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Set Flask and Svelete host - Allows for localhost in ECS and Container names locally
-echo "Setting $FLASK_HOST and $SVELTE_HOST in Nginx configuration..."
-envsubst '$FLASK_HOST $SVELTE_HOST' </etc/nginx/nginx.template.conf >/etc/nginx/nginx.conf
+echo "Setting $FLASK_SERVICE_HOST and $SVELTE_SERVICE_HOST in Nginx configuration..."
+envsubst '$FLASK_SERVICE_HOST $SVELTE_SERVICE_HOST' </etc/nginx/nginx.template.conf >/etc/nginx/nginx.conf
 
 # Start nginx
 echo "Starting Nginx..."
