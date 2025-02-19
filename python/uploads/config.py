d508f889df0ae0c69a2a7689aba7606f84e40e23
Brandon Lefore <brandon.lefore@nivalta.com>
2025-02-19 13:19:20 -0800

    Remove redis from uploads config

--- python/uploads/config.py
+++ python/uploads/config.py
@@ -7,5 +7,3 @@ class LocalFlaskConfig:
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://local_uploads_user:local_uploads_pass@local-uploads-db:3306/uploads_db"
     UPLOAD_DIRECTORY = "/usr/src/uploads/uploads"
     MAX_CONTENT_LENGTH = 536_870_912  # 512MB in bytes
-    REDIS_HOST = "redis"
-    REDIS_PORT = 6379
